5a0133678a2279504132f092dcb4bef7f0742724
rodrigo <ro.aedo@duocuc.cl>
2026-06-30 09:11:15 -0400

    fix: simplify CSV loading function call by removing unnecessary parameters

--- src/pipeline.py
+++ src/pipeline.py
@@ -68,7 +68,7 @@ def main():
         logger.info(f'Modo OneDrive, se generar archivo en Directorio data/raw/ {ruta_archivo}')
     
     # Ingesta
-    df = cargar_csv(ruta_archivo,sep=None,engine='PYTHON')
+    df = cargar_csv(ruta_archivo)
     logger.info(f'Arvhivo cargado: {len(df)} filas' if df is not None else "Archivo vacío o no encontrado")
     if df is None:
         logger.error("No se puede cargar el csv")
